cf9f3146fd64d8f48eda55915ee95497874de623
ajay-muchalambe <80315625+ajay-muchalambe@users.noreply.github.com>
2022-07-17 11:12:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
-from flask import Flask, render_template, request,jsonify
-from flask_cors import CORS,cross_origin
-import requests
-from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
+
+import pymongo
+import requests
+import mysql.connector
+from bs4 import BeautifulSoup as bs
+from flask import Flask, render_template, request
+from flask_cors import cross_origin
+
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET'])  # route to display the home page
+
+@app.route('/', methods=['GET'])  # route to display the home page
 @cross_origin()
 def homePage():
     return render_template("index.html")
 
-@app.route('/review',methods=['POST','GET']) # route to show the review comments in a web UI
+
+@app.route('/review', methods=['POST', 'GET'])  # route to show the review comments in a web UI
 @cross_origin()
 def index():
     if request.method == 'POST':
         try:
-            searchString = request.form['content'].replace(" ","")
+            searchString = request.form['content'].replace(" ", "")
             flipkart_url = "https://www.flipkart.com/search?q=" + searchString
             uClient = uReq(flipkart_url)
             flipkartPage = uClient.read()
@@ -26,9 +32,9 @@ def index():
             del bigboxes[0:3]
             box = bigboxes[0]
             productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
-            #print(productLink)
+            # print(productLink)
             prodRes = requests.get(productLink)
-            prodRes.encoding='utf-8'
+            prodRes.encoding = 'utf-8'
             prod_html = bs(prodRes.text, "html.parser")
             print(prod_html)
             commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
@@ -40,14 +46,14 @@ def index():
             reviews = []
             for commentbox in commentboxes:
                 try:
-                    #name.encode(encoding='utf-8')
+                    # name.encode(encoding='utf-8')
                     name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
 
                 except:
                     name = 'No Name'
 
                 try:
-                    #rating.encode(encoding='utf-8')
+                    # rating.encode(encoding='utf-8')
                     rating = commentbox.div.div.div.div.text
 
 
@@ -55,30 +61,55 @@ def index():
                     rating = 'No Rating'
 
                 try:
-                    #commentHead.encode(encoding='utf-8')
+                    # commentHead.encode(encoding='utf-8')
                     commentHead = commentbox.div.div.div.p.text
 
                 except:
                     commentHead = 'No Comment Heading'
                 try:
                     comtag = commentbox.div.div.find_all('div', {'class': ''})
-                    #custComment.encode(encoding='utf-8')
+                    # custComment.encode(encoding='utf-8')
                     custComment = comtag[0].div.text
                 except Exception as e:
-                    print("Exception while creating dictionary: ",e)
+                    print("Exception while creating dictionary: ", e)
 
                 mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
                           "Comment": custComment}
-                reviews.append(mydict)
-            return render_template('results.html', reviews=reviews[0:(len(reviews)-1)])
+                # Below code snipped adds customer reviews to Mongo collection
+
+                my_client = pymongo.MongoClient("mongodb://localhost:27017/")
+                mydb = my_client["webscrapper"]
+                my_col = mydb[searchString]
+
+                my_col.insert_one(mydict)
+
+                mydb = mysql.connector.connect(
+                    host="localhost",
+                    user="root",
+                    password="XXXX",
+                    database="webscrapper"
+                )
+
+            my_cursor = mydb.cursor()
+
+            ins_qry = "INSERT INTO REVIEWS ({columns}) VALUES {values};".format(
+                    columns=', '.join(mydict.keys()),
+                    values=tuple(mydict.values())
+                )
+            my_cursor.execute(ins_qry)
+
+            reviews.append(mydict)
+
+            return render_template('results.html', reviews=reviews[0:(len(reviews) - 1)])
         except Exception as e:
-            print('The Exception message is: ',e)
+            print('The Exception message is: ', e)
             return 'something is wrong'
     # return render_template('results.html')
 
     else:
         return render_template('index.html')
 
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8001, debug=True)
-	#app.run(debug=True)
+# app.run(debug=True)
